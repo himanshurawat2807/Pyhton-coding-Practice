4f9de2f77568087b6ca85ffaa70f6a58b561be99
himanshurawat2807 <61087860+himanshurawat2807@users.noreply.github.com>
2023-07-17 21:06:03 +0530

    Created using Colaboratory

---
+++ Print_Hello.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOdYA3ImQbBZaVA1B9afnDc",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/himanshurawat2807/Pyhton-coding-Practice/blob/main/Print_Hello.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "HI7bAN5BfAQM",
+        "outputId": "674ef81a-35a0-4cf7-ba91-f19e18ae91cd"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello\n"
+          ]
+        }
+      ],
+      "source": [
+        "# Q print hello\n",
+        "\n",
+        "print('hello')"
+      ]
+    }
+  ]
+}
